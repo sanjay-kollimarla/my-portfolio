3e116db0d79e98daa4f10c3c07f15ffd5eff9d93
sanjay-kollimarla <sanju.koli2500@gmail.com>
2025-08-11 04:02:39 +0530

    ,

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -24,11 +24,11 @@ export default function Nav(){
       <div className='container nav-inner'>
         <div className='brand'><i class="bi bi-file-code fs-2 cursor-pointer"></i></div>
         <nav className='nav-links ms-auto'>
-          <a href="#about">About</a>
-          <a href="#skills">Skills</a>
-          <a href="#projects">Projects</a>
-          <a href="#certs">Certificates</a>
-          <a href="#contact">Contact</a>
+          <a href="#about" className="nav-link">About</a>
+          <a href="#skills" className="nav-link">Skills</a>
+          <a href="#projects" className="nav-link">Projects</a>
+          <a href="#certs" className="nav-link">Certificates</a>
+          <a href="#contact" className="nav-link">Contact</a>
         </nav>
         {/* <div className='nav-actions'>
           <a href="https://github.com/yourusername" target='_blank' rel='noreferrer'><FiGithub size={18} /></a>
