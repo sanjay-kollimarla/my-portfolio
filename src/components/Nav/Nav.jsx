122d81ebeb568802725e272890a75fddb8d71582
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-08-13 21:24:14 +0530

    fixed the navbar icon

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -76,8 +76,8 @@ export default function Nav() {
   return (
     <header className="nav">
       <div className="container nav-inner">
-        <a className="brand" href="">
-          <i className="bi bi-file-code fs-2 text-white"></i>
+        <a className="brand" href="#home">
+          <i className="bi bi-file-code text-white"></i>
         </a>
         <button
           className={`hamburger ${isMenuOpen ? "nav-open" : ""}`}
